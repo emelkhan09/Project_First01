f6e69fb321f74fd2e885566f84eb7ed2df83d67b
Me <khad.emel@yandex.ru>
2020-09-23 15:02:47 +0300

    Initial Commit

---
+++ Project_First/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project_First
+//
+//  Created by test on 23.09.2020.
+//  Copyright © 2020 Emelkhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

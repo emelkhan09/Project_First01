873dc8d09055bfc9b8b73b29d0fd81dcc42f2fe0
Me <khad.emel@yandex.ru>
2020-09-28 20:55:25 +0300

    Added applySnapshot

--- Project_First/ViewController.swift
+++ Project_First/ViewController.swift
@@ -11,18 +11,46 @@ import UIKit
 class ListController: UICollectionViewController {
     
     var repositaries = ["Alamofire","Realm","KingFisher","SnapKit"]
+    private enum Section: CaseIterable {
+    case main
+    }
+    private lazy var dataSource: UICollectionViewDiffableDataSource<Section, [String]> = {
+        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, [String]>
+          { cell, indexPath, repositary in
+            var content = cell.defaultContentConfiguration()
+            content.text = repositary
+            cell.contentConfiguration = content
+        }
+
+
+    return UICollectionViewDiffableDataSource<Section, [String]>(collectionView: collectionView)
+      { (collectionView, indexPath, repositary) -> UICollectionViewCell? in
+    collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: repositary)
+      }
+    }()
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+        private func applySnapshot(animatingDifferences: Bool = true) {
+          var snapshot = NSDiffableDataSourceSnapshot<Section, [String]>()
+          snapshot.appendSections(Section.allCases)
+          snapshot.appendItems(repositaries)
+          dataSource.apply(snapshot, animatingDifferences: animatingDifferences)
+        }
         
         var config = UICollectionLayoutListConfiguration(appearance:
           .insetGrouped)
         config.backgroundColor = .systemPurple
         collectionView.collectionViewLayout =
           UICollectionViewCompositionalLayout.list(using: config)
-    }
-    private enum Sector: CaseIterable {
-        case main
-    }
+        
+
+        
+
 
 }
+   
+
 

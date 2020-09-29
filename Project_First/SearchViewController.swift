d95f1a0fef367f7c9b27964cab001fd2c93d61eb
Me <khad.emel@yandex.ru>
2020-09-29 15:45:33 +0300

    Fixed function

--- Project_First/SearchViewController.swift
+++ Project_First/SearchViewController.swift
@@ -50,8 +50,7 @@ final class SearchViewController: UIViewController {
         view.addSubview(collectionView)
         title = "Search"
         applySnapshot(animatingDifferences: false)
-        
-    }
+        }
     
 }
 private extension SearchViewController {

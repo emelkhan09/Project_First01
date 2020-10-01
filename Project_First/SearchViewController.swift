74b293a22febe7fac98a11d4606c1fadcedea38f
Me <khad.emel@yandex.ru>
2020-10-01 16:22:41 +0300

    Add filteredRepositories

--- Project_First/SearchViewController.swift
+++ Project_First/SearchViewController.swift
@@ -14,6 +14,7 @@ final class SearchViewController: UIViewController {
         case main
     }
     
+    var filteredRepositories: String = ""
     private let repositories = ["Alamofire", "Realm", "KingFisher", "SnapKit"]
     private let searchController = UISearchController(searchResultsController: nil)
     private lazy var collectionView: UICollectionView = {
